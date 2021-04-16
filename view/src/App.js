2b0521ed38b4f888b0b4462fd93f909c5958830e
wally_yoon <bluegmlduf2@naver.com>
2021-04-16 23:54:42 +0900

    0416 form , card 레이아웃 구현

--- view/src/App.js
+++ view/src/App.js
@@ -1,27 +1,29 @@
 /*eslint-disable*/
 import react, { useState } from 'react';
-import { Container, Row, Nav, Col, Button } from 'react-bootstrap'; // npm install react-bootstrap bootstrap
+import { Container, Row, Card, CardDeck, Col, Button, Form } from 'react-bootstrap'; // npm install react-bootstrap bootstrap
 import './css/App.css';
 import './css/_reset.css';
 import './css/content.css';
 import './css/bootstrap.min.css'
 
 function App() {
+  let [roomInfo, roomUpd] = useState([1, 4, 5, 6])
+
   return (
     <Container fluid className="App">
-      <Row className="nav p-2 text-center btn">
+      <Row className="nav p-2 text-center">
         <Col className="logo" md={3}>
           <img src="./logo2.png" width="45px" />
-          <h3>이방저방</h3>
+          <h3>BANGMON</h3>
         </Col>
         <Col md={{ span: "2" }} className="">
-          <Button variant="/home" size="lg">지도</Button>
+          <Button variant="/home" size="lg">マップ</Button>
         </Col>
         <Col md={{ span: "2" }}>
-          <Button variant="/home" size="lg">관심목록</Button>
+          <Button variant="/home" size="lg">お気に入り</Button>
         </Col>
         <Col md={{ span: "2" }}>
-          <Button variant="/home" size="lg">방내놓기</Button>
+          <Button variant="/home" size="lg">売る</Button>
         </Col>
         <Col md={3}>
           <Button variant="" className="btn_outline" size="lg">회원가입</Button>
@@ -29,14 +31,49 @@ function App() {
         </Col>
       </Row>
 
-      <Row className="serach">
-
+      <Row className="serach justify-content-md-center align-items-center">
+        <Col md="8" >
+          <Form>
+            <Form.Row>
+              <Form.Group as={Col} controlId="formGridEmail">
+                <Form.Label className="label_white xl"><h2>どんな部屋をお探しですか？</h2></Form.Label>
+                <Form.Control type="text" className="h-100" placeholder="探す場所を入力して下さい。" />
+              </Form.Group>
+            </Form.Row>
+          </Form>
+        </Col>
       </Row>
 
+      <Row className="roomList_label justify-content-md-center">
+        <Col md="8">
+          <h2><span className={"lable_bold"}>おすすめ</span><span>の部屋</span></h2>
+        </Col>
+      </Row>
 
-      <div className="roomList">
+      <Row className="roomList justify-content-md-center">
+        <Col md="8">
+        <CardDeck>
+          {roomInfo.map((e, i) => {
+            return (
+              <Col md="3" className="">
+                <Card>
+                  <Card.Img variant="top" src="./cardImg.svg" />
+                  <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Text>
+                      Some quick example text to build on the card title and make up the bulk of
+                      the card's content.
+                    </Card.Text>
+                    <Button variant="primary">Go somewhere</Button>
+                  </Card.Body>
+                </Card>
+              </Col>
+            )
+          })}
+        </CardDeck>
+        </Col>
+      </Row>
 
-      </div>
       <div className="apartList">
 
       </div>
